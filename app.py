38327147aa6fc036fbafbf0edf05859f57fa859a
rewop <dyalist2001@gmail.com>
2024-01-11 13:21:58 +0200

    added new file with class of User and changed extensions.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 from flask import Flask
+from extensions import db
 
 app = Flask(__name__)
 
-app.config['FLASK_SECRET_KEY'] = 'AIEUTGHWEUIFKOASFO'
+app.config['FLASK_SECRET_KEY'] = 'A1EU4GHWeUIfkOASF0'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
 
 
 
