980b8d8b872dc84e5658083469a47d3e11b76705
rewop <dyalist2001@gmail.com>
2024-01-11 14:02:03 +0200

    added db and ability to registration

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from extensions import db
+from auth import auth_bp
 
 app = Flask(__name__)
 
@@ -7,8 +8,11 @@ app.config['FLASK_SECRET_KEY'] = 'A1EU4GHWeUIfkOASF0'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SQLALCHEMY_ECHO'] = True
 
+#initialize extensions
 db.init_app(app)
 
+#register blueprints
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 
 if __name__ == '__main__':
